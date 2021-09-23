3c2ab32b45af570db68e076e7d6bbff8e4606230
Garrett Pipes <garrett.pipes@gmail.com>
2021-09-23 16:31:55 -0400

    created bootMVC project

---
+++ ActivePotato/src/main/java/com/skilldistillery/activepotato/ActivePotatoApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.activepotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivePotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivePotatoApplication.class, args);
+	}
+
+}
---
+++ ActivePotato/src/test/java/com/skilldistillery/activepotato/ActivePotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.activepotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivePotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
