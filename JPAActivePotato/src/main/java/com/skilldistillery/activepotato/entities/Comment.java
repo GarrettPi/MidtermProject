ab6bc1fede55f04747687cf8f14df4ad669ad6b0
ronald-viramontes <ronald.viramontes@gmail.com>
2021-09-24 15:27:17 -0400

    add Activity & Comment

---
+++ JPAActivePotato/src/main/java/com/skilldistillery/activepotato/entities/Comment.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.activepotato.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	
+}
