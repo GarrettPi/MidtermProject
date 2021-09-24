7b4480a9ba91d6f822dcad3a682d92aad7aec45d
ronald-viramontes <ronald.viramontes@gmail.com>
2021-09-24 16:54:38 -0400

    ManyToOne Comment & User

--- JPAActivePotato/src/main/java/com/skilldistillery/activepotato/entities/Comment.java
+++ JPAActivePotato/src/main/java/com/skilldistillery/activepotato/entities/Comment.java
@@ -27,6 +27,18 @@ public class Comment {
 	@JoinColumn(name="activity_id")
 	private Activity activity;
 
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	public Activity getActivity() {
 		return activity;
 	}
