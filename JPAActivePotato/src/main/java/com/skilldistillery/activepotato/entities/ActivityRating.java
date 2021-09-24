c735233fd1e495aad929384d0128e6150e13e146
GarrettPi <86500870+GarrettPi@users.noreply.github.com>
2021-09-24 16:04:06 -0400

    activity rating entitiy built

---
+++ JPAActivePotato/src/main/java/com/skilldistillery/activepotato/entities/ActivityRating.java
@@ -0,0 +1,75 @@
+package com.skilldistillery.activepotato.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="activity_rating")
+@Entity
+public class ActivityRating {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String url;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "ActivityRating [id=" + id + ", name=" + name + ", url=" + url + "]";
+	}
+
+	public ActivityRating() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ActivityRating other = (ActivityRating) obj;
+		return id == other.id;
+	}
+	
+	
+
+}
