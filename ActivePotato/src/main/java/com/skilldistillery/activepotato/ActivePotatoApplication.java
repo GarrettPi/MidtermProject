ed122f863805c6dd1caf44aa652a6790d0c9895e
jttweedy11 <87153150+jttweedy11@users.noreply.github.com>
2021-09-30 17:33:16 -0500

    Configure boot app for war deployment

--- ActivePotato/src/main/java/com/skilldistillery/activepotato/ActivePotatoApplication.java
+++ ActivePotato/src/main/java/com/skilldistillery/activepotato/ActivePotatoApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.activepotato;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActivePotatoApplication {
+public class ActivePotatoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ActivePotatoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivePotatoApplication.class, args);
